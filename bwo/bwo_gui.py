a33de9f0284d2c884a6edf2b02365f63004d84a0
adonohue02 <48253815+adonohue02@users.noreply.github.com>
2024-10-09 19:51:12 -0400

    added labels for where urls will be positioned

--- bwo/bwo_gui.py
+++ bwo/bwo_gui.py
@@ -19,7 +19,7 @@ def open_with_hotkey():
     if current_hotkey: #remove current hotkey
         rm_hotkey()
 
-    urls = [format_url(url_entry1.get()),format_url(url_entry2.get()),format_url(url_entry3.get())]
+    urls = [format_url(url_entry3.get()),format_url(url_entry1.get()),format_url(url_entry2.get())]
 
     create_hotkey(urls) # use hot keys to open entered urls
 
@@ -35,25 +35,45 @@ def create_gui():
 
     global url_entry1, url_entry2, url_entry3
 
-    url_entry1 = tk.Entry(root, width=50, font = ("Arial", 12))
+#url label 1 
+    url_label1 = tk.Label(root, text="URL 1 (Left Long Window):", font=("Arial", 10))
+    url_label1.pack(pady=5)
+    
+# url entry 1
+    url_entry1 = tk.Entry(root, width=50, font=("Arial", 12))
     url_entry1.pack(pady=5)
 
-    url_entry2 = tk.Entry(root, width=50, font = ("Arial", 12))
+#url label 2
+    url_label2 = tk.Label(root, text="URL 2 (Top Right Window):", font=("Arial", 10))
+    url_label2.pack(pady=5)
+   
+# url entry 2
+    url_entry2 = tk.Entry(root, width=50, font=("Arial", 12))
     url_entry2.pack(pady=5)
 
-    url_entry3 = tk.Entry(root, width=50, font = ("Arial", 12))
+#url label 3
+    url_label3 = tk.Label(root, text="URL 3 (Bottom Right Window):", font=("Arial", 10))
+    url_label3.pack(pady=5)
+
+# url entry 3
+    url_entry3 = tk.Entry(root, width=50, font=("Arial", 12))
     url_entry3.pack(pady=5)
 
+
+# instruction window
     instruction_button = tk.Button(root, text="Show Instructions", command=show_instructions, font=("Arial", 12))
     instruction_button.pack(pady=20)
 
+#hotkey button
     hotkey_button = tk.Button(root, text = "Activate Hotkeys", command=open_with_hotkey, font= ("Arial, 12"))
     hotkey_button.pack(pady=10)
 
+#exit button
     exit_button = tk.Button(root, text="Exit", command=root.quit, font=("Arial", 12))
     exit_button.pack(pady=10)
 
-    root.protocol("WM_DELETE_WINDOW", root.quit)  # Ensure proper exit
+#exit button functionality
+    root.protocol("WM_DELETE_WINDOW", root.quit)  
     root.mainloop()
 
 def show_instructions():
